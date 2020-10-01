10026daad5ada666e39d9f82a14446c0b7b2c66f
EC2 Default User <ec2-user@ip-172-31-83-138.ec2.internal>
2020-10-01 13:30:20 +0000

    Redefined nasa onclick method in app.js

--- public/server/app.js
+++ public/server/app.js
@@ -2,6 +2,43 @@ let https = new XMLHttpRequest();
 
 var nasaImg = document.getElementById('nasaImg');
 var nasaDate = document.getElementById('dateNasa');
+var nasaBtn = document.getElementById('nasabtn');
+
+nasaBtn.onclick = function () {
+  
+  var d = new Date(nasaDate.value);
+    
+    
+    
+    var year = d.getFullYear();
+    var day = d.getDate();
+    var month = d.getMonth()+1;
+    
+   
+    
+    
+    var getYear = "date=" + year + "-" + month + "-" + day;
+    
+    https.open("GET", "https://api.nasa.gov/planetary/apod?api_key=zuOehZm6hq1Rt9cufqw36ADo0kZT3mfq5dGmCPNo");
+    https.send();
+  
+    https.onload = () => {
+      
+    if(https.status === 200){
+      var vals = JSON.parse(https.response);
+    }else{
+      console.log(`error ${https.status} ${https.statusText}`);
+    }
+    
+    console.log(vals);
+    console.log(vals["url"]);
+    console.log(vals["explanation"]);
+    
+    nasaImg.src = vals["url"];
+    nasaImg.style.width = '500px';
+    nasaImg.style.height = 'auto';
+}
+}
 
 function getNasa () { //USING XMLHttpRequest
   
