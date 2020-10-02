54535d125a2cbbb24863d2a2c94671f049107fee
EC2 Default User <ec2-user@ip-172-31-83-138.ec2.internal>
2020-10-02 06:14:20 +0000

    Rewrote nasa api method in app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,3 @@
-
-import fetch from 'cross-fetch';
 let https = new XMLHttpRequest();
 
 var nasaImg = document.getElementById('nasaImg');
@@ -8,35 +6,14 @@ var nasaBtn = document.getElementById('nasabtn');
 
 
 nasaBtn.onclick = function () {
-  
-   fetch('https://api.nasa.gov/planetary/apod?api_key=zuOehZm6hq1Rt9cufqw36ADo0kZT3mfq5dGmCPNo')
-  .then(res => {
-    if (res.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-    
-    return res.json();
-  })
-  .then(user => {
-    console.log(user);
-  })
-  .catch(err => {
-    console.error(err);
-  });
-}
 
-function getNasa () { //USING XMLHttpRequest
+//function getNasa () { //USING XMLHttpRequest
   
     var d = new Date(nasaDate.value);
-    
-    
     
     var year = d.getFullYear();
     var day = d.getDate();
     var month = d.getMonth()+1;
-    
-   
-    
     
     var getYear = "date=" + year + "-" + month + "-" + day;
     
@@ -51,9 +28,9 @@ function getNasa () { //USING XMLHttpRequest
       console.log(`error ${https.status} ${https.statusText}`);
     }
     
-    console.log(vals);
-    console.log(vals["url"]);
-    console.log(vals["explanation"]);
+   // console.log(vals);
+   // console.log(vals["url"]);
+   // console.log(vals["explanation"]);
     
     nasaImg.src = vals["url"];
     nasaImg.style.width = '500px';
