ff84bf57ef54c8d7cb7b8ee8812c02c7227f2021
EC2 Default User <ec2-user@ip-172-31-83-138.ec2.internal>
2020-10-06 13:00:51 +0000

    Rearranged referencing

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -202,3 +202,36 @@ wet.onload = () => {
 
 */
     
+
+
+//Email
+
+
+var emailBtn = document.getElementById('emailBtn');
+
+
+function sendEmail(){
+    
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey("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")
+    const msg = {
+      to: 'test@example.com', // Change to your recipient
+      from: 'test@example.com', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    
+}
+
+emailBtn.onclick = function () {
+    sendEmail();
+  }
